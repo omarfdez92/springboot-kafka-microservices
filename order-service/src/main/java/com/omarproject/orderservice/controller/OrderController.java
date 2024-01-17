8fd669725f8faa12daf0f8e7bd853af5ddc8a5fb
omarfdez92 <omarfdez92@gmail.com>
2024-01-17 00:29:06 -0600

    Initial commit

---
+++ order-service/src/main/java/com/omarproject/orderservice/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.omarproject.orderservice.controller;
+
+import com.omarproject.basedomains.dto.Order;
+import com.omarproject.basedomains.dto.OrderEvent;
+import com.omarproject.orderservice.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully ...";
+    }
+
+}
